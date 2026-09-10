4f22a09339f5e453876304ffefd533c9079a131b
KNSBA <phamhoangan@zenga.com.vn>
2026-09-10 10:15:10 +0700

    add enemies prefabs

--- ARCHEROmini/Assets/Project/Scripts/Enemies/Type/Planta.cs
+++ ARCHEROmini/Assets/Project/Scripts/Enemies/Type/Planta.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class DarkBee : MonoBehaviour
+public class Planta : BaseMonster
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
