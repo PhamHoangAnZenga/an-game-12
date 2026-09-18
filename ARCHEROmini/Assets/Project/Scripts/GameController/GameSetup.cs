19a0ec0b5aae1b09205c145d75e378f8d418a9ef
KNSBA <phamhoangan@zenga.com.vn>
2026-09-18 13:45:28 +0700

    add gpu batch

---
+++ ARCHEROmini/Assets/Project/Scripts/GameController/GameSetup.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    // public GameObject[] targets;
+    // void Awake()
+    // {
+    //     Application.targetFrameRate = 60;
+    //     CheckPropertyBlock();
+    // }
+
+    // void CheckPropertyBlock()
+    // {
+    //     foreach (var target in targets)
+    //     {
+    //         Renderer rend = target.GetComponent<Renderer>();
+    //         Debug.Log(target.name + " " + rend.HasPropertyBlock());
+    //     }
+    // }
+}
