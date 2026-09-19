1220fa5dd561781cd6693a175ebf8ba6fc60957c
KNSBA <phamhoangan@zenga.com.vn>
2026-09-19 11:45:51 +0700

    optimize UI drawcall

--- ARCHEROmini/Assets/Project/Scripts/GameController/GameSetup.cs
+++ ARCHEROmini/Assets/Project/Scripts/GameController/GameSetup.cs
@@ -2,19 +2,8 @@ using UnityEngine;
 
 public class GameSetup : MonoBehaviour
 {
-    // public GameObject[] targets;
-    // void Awake()
-    // {
-    //     Application.targetFrameRate = 60;
-    //     CheckPropertyBlock();
-    // }
-
-    // void CheckPropertyBlock()
-    // {
-    //     foreach (var target in targets)
-    //     {
-    //         Renderer rend = target.GetComponent<Renderer>();
-    //         Debug.Log(target.name + " " + rend.HasPropertyBlock());
-    //     }
-    // }
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
 }
